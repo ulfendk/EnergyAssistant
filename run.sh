e941bf48bff61b5b81167fbba6496854e6b4541e
Regin Ulf Nielsen <regin@ulfen.dk>
2022-08-20 08:04:38 +0200

    Started on run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/with-contenv bashio
 
+CONFIG="setting.config"
+
+bashio::log.info "Creating Energy Assistant configuration..."
+
+# Create main config
 USER_NAME=$(bashio::config 'user')
 API_KEY=$(bashio::config 'api_key')
 
-echo "Hello world!"
+# DEFAULT_LEASE=$(bashio::config 'default_lease')
+# DNS=$(bashio::config 'dns|join(", ")')
+# DOMAIN=$(bashio::config 'domain')
+# MAX_LEASE=$(bashio::config 'max_lease')
+
+{
+    echo "{"
+    echo "  \"@username\": \"${USER_NAME}\","
+    echo "  \"apikey\": \"${API_KEY},"
+    echo "}"
+} > "${CONFIG}"
+
+./EnergyAssistant
