d4a5bc5f9b2fe15013165ecff6e98b33a7af0a4f
Regin Nielsen <ulfendk@users.noreply.github.com>
2023-11-13 23:37:55 +0100

    New fee model again

--- src/EnergyAssistantLib/Database/EnergyAssistantContext.cs
+++ src/EnergyAssistantLib/Database/EnergyAssistantContext.cs
@@ -1,7 +1,78 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 
 namespace UlfenDk.EnergyAssistant.Database;
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple=false, Inherited=false)]
+public class UnitOfMeasurementAttribute : Attribute
+{
+    public string UnitOfMeasurement { get; }
+
+    public UnitOfMeasurementAttribute(string unitOfMeasurement)
+    {
+        UnitOfMeasurement = unitOfMeasurement;
+    }
+}
+
+public enum NewFeeApplication
+{
+    [UnitOfMeasurement("DKK")]
+    Yearly,
+    
+    [UnitOfMeasurement("DKK")]
+    Monthly,
+    
+    [UnitOfMeasurement("DKK / kWh")]
+    AlwaysPerUnit,
+    
+    [UnitOfMeasurement("DKK / kWh")]
+    RegularFeePerUnit,
+
+    [UnitOfMeasurement("DKK / kWh")]
+    ReducedFeePerUnit
+}
+
+public class NewFee
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+
+    public NewFeeApplication Application { get; set; }
+
+    public string Name { get; set; }
+
+    public DateOnly PeriodStart { get; set; }
+
+    public DateOnly PeriodEnd { get; set; }
+
+    public TimeOnly? DailyIntervalStart { get; set; }
+
+    public decimal Amount { get; set; }
+
+    [NotMapped]
+    public bool IsValid => Application switch
+    {
+        NewFeeApplication.Yearly => IsPeriodValid && DailyIntervalStart is null,
+        NewFeeApplication.Monthly => IsPeriodValid && DailyIntervalStart is null,
+        NewFeeApplication.AlwaysPerUnit => IsPeriodValid,
+        NewFeeApplication.RegularFeePerUnit => IsPeriodValid,
+        NewFeeApplication.ReducedFeePerUnit => IsPeriodValid,
+        _ => false
+    };
+    
+    [NotMapped]
+    public string UnitOfMeasurement =>
+        typeof(NewFeeApplication).GetCustomAttribute(typeof(UnitOfMeasurementAttribute)) switch
+        {
+            UnitOfMeasurementAttribute uom => uom.UnitOfMeasurement,
+            _ => string.Empty
+        };
+    
+    private bool IsPeriodValid => PeriodStart < PeriodEnd;
+}
+
 
 public class YearPeriod
 {
